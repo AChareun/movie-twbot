b49d83f8ab890cb2745202b7833199932557a604
AChareun <achareun22@gmail.com>
2020-07-13 13:13:11 -0300

    add successfull requests to tmdb api to get a random movie

--- src/api/get-data.js
+++ src/api/get-data.js
@@ -1,21 +1,30 @@
 import { TMDBKEYS } from "../config";
-import request from "request";
+import fetch from "node-fetch";
 
 const API_KEY = `api_key=${TMDBKEYS.API_KEY}`;
 const BASE_URL = "https://api.themoviedb.org/3/discover/movie?";
 
-const requestMovieList = () => {
-  request(
-    `${BASE_URL}${API_KEY}&language=en-US&include_adult=false&include_video=false&page=1`,
-    (err, res, body) => {
-      if (!err) {
-        const data = JSON.parse(body)
-        console.log(data.results[0].genre_ids);
-      } else {
-        console.error(err);
-      }
-    }
-  );
+const requestMovieList = async (page = 1) => {
+  const MOVIE_LIST = await fetch(
+    `${BASE_URL}${API_KEY}&language=en-US&include_adult=false&include_video=false&page=${page}`
+  )
+  .then(res => res.json());
+
+  return MOVIE_LIST;
 };
 
-requestMovieList();
+const requestARandomMovie = async () => {
+  const TOTAL_PAGES = await requestMovieList().then(res => res.total_pages);
+
+  const MOVIE_LIST = await requestMovieList(Math.floor(Math.random() * TOTAL_PAGES));
+
+  const RANDOM_MOVIE = MOVIE_LIST.results[Math.floor(Math.random() * MOVIE_LIST.results.length)];
+
+  const movieData = await fetch(`https://api.themoviedb.org/3/movie/${RANDOM_MOVIE.id}?${API_KEY}&language=en-US`)
+  .then(res => res.json())
+
+  console.log(movieData);
+  return movieData;
+}
+
+requestARandomMovie();
