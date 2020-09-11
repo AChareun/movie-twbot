ef470da8f6f2d5566228e2c5f5fb269455aff5f6
AChareun <achareun22@gmail.com>
2020-09-10 23:37:46 -0300

    refactors MovieService so it no longer makes the api calls

--- src/module/movie/service/movieService.js
+++ src/module/movie/service/movieService.js
@@ -1,6 +1,5 @@
 /* eslint-disable class-methods-use-this */
 
-const fetch = require('node-fetch');
 const mapMovie = require('../mapper/movieMapper');
 
 /**
@@ -18,36 +17,19 @@ module.exports = class MovieService {
   }
 
   /**
-   * @param {string} url
-   * @returns {JSON}
-   */
-  async fetchData(url) {
-    const data = await fetch(url)
-      .then((res) => res.json());
-    return data;
-  }
-
-  /**
    * @param {string} request
-   * @returns {JSON} Data of a random movie with the params passed
-   */
-  async getRandomMovieData(request) {
-    const { total_pages: totalPages } = await this.fetchData(this.movieApi.getUrl(request, 1));
-
-    const randomPage = Math.floor(Math.random() * totalPages + 1);
-    const movieList = await this.fetchData(this.movieApi.getUrl(request, randomPage));
-
-    const randomMovie = movieList.results[Math.floor(Math.random() * movieList.results.length + 1)];
-    const movieData = await this.fetchData(this.movieApi.getUrl(request, null, randomMovie.id));
-
-    return movieData;
-  }
-
-  /**
    * @returns {Movie} Mapped Movie Object with fetched/fallback data
    */
   async getMovie(request) {
-    const newMovie = await this.getRandomMovieData(request);
-    return mapMovie(newMovie);
+    const { total_pages: totalPages } = await this.movieApi.getMovieData(request, 1);
+
+    const randomPage = Math.floor(Math.random() * totalPages + 1);
+    const movieList = await this.movieApi.getMovieData(request, randomPage);
+
+    const randomMovie = movieList.results[Math.floor(Math.random() * movieList.results.length + 1)];
+    const movieData = await this.movieApi.getMovieData(request, null, randomMovie.id);
+
+    const movie = mapMovie(movieData);
+    return movie;
   }
 };
