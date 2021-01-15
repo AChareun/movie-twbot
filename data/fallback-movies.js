9c03c39eeb39301ee2377bedc88f617ec4a78e3a
AChareun <achareun22@gmail.com>
2021-01-14 23:41:58 -0300

    relocate fallback movie data

---
+++ data/fallback-movies.js
@@ -0,0 +1,40 @@
+const fallbackMovies = [
+    {
+        id: 1,
+        imdb: 'tt1440292',
+        title: 'Submarine',
+        original_title: 'Submarine',
+        overview:
+            'Un adolescente de 15 años tiene dos objetivos: perder su virginidad antes de su cumpleaños y extinguir la llama entre su madre y un ex-amante que ha reaparecido en su vida',
+        original_language: 'en',
+        genres: ['Comedia', 'Drama', 'Romance'],
+        runtime: 97,
+        release_date: '2010',
+    },
+    {
+        id: 2,
+        imdb: 'tt1022603',
+        title: '500 Days of Summer',
+        original_title: '500 Days of Summer',
+        overview:
+            'Una excéntrica comedia romántica acerca de una mujer que no cree en el amor y el hombre que se enamora de ella',
+        original_language: 'en',
+        genres: ['Comedia', 'Drama', 'Romance'],
+        runtime: 95,
+        release_date: '2009',
+    },
+    {
+        id: 3,
+        imdb: 'tt2582802',
+        title: 'Whiplash',
+        original_title: 'Whiplash',
+        overview:
+            'Un joven y prometedor baterista se inscribe en un conservatorio donde sus sueños son guiados por un instructor que no se detendrá ante nada para realizar el potencial de un estudiante',
+        original_language: 'en',
+        genres: ['Drama', 'Musica'],
+        runtime: 106,
+        release_date: '2014',
+    },
+];
+
+export default fallbackMovies;
