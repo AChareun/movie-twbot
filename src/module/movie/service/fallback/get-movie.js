3318e9f470ce854ed98f3daec8cce3aea263722b
AChareun <achareun22@gmail.com>
2021-01-14 23:46:51 -0300

    delete old files

--- src/module/movie/service/fallback/get-movie.js
+++
@@ -1,9 +0,0 @@
-import movies from './fallback-movies';
-
-const getFallbackMovie = () => {
-    const movieData = movies[Math.floor(Math.random() * movies.length - 1)];
-
-    return movieData;
-};
-
-export default getFallbackMovie;
